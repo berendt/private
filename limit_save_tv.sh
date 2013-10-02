5c3ace716a941e8a6187de0acb79193ccd14f5d2
Christian Berendt <berendt@b1-systems.de>
2013-10-02 17:30:27 +0200

    script to limit the bandwidth used for save.tv

---
+++ limit_save_tv.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+interface=wlp3s0
+range=78.152.40.0-78.152.40.254
+limit=6Mbit
+
+sudo iptables -t mangle -L POSTROUTING | grep --quiet $range
+if [[ $? -ne 0 ]]; then
+    sudo iptables -t mangle -A POSTROUTING -o $interface -p tcp -m iprange --dst-range $range -j CLASSIFY --set-class 2:11
+fi
+
+sudo tc qdisc delete dev $interface root handle 2:0
+sudo tc qdisc add dev $interface root handle 2:0 htb default 99
+sudo tc class add dev $interface parent 2:0 classid 2:1 htb rate 100Mbit ceil 100Mbit
+sudo tc class add dev $interface parent 2:1 classid 2:11 htb rate $limit ceil $limit prio 2
+sudo tc qdisc add dev $interface parent 2:11 handle 20: sfq perturb 10
