6e8898fd4a3e65d7c1a3f0c55c1af21c4ab2627a
Christian Berendt <berendt@b1-systems.de>
2013-10-11 13:41:55 +0200

    fixed

--- ceph/cleanup.sh
+++ ceph/cleanup.sh
@@ -4,15 +4,19 @@ sudo stop ceph-all
 ssh ceph002 'sudo stop ceph-all'
 ssh ceph003 'sudo stop ceph-all'
 ceph-deploy purge ceph001 ceph002 ceph003
-sudo rm -rf /var/lib/ceph
-sudo rm -rf /etc/ceph
-sudo rm -rf /var/log/ceph
-ssh ceph002 'sudo rm -rf /var/lib/ceph'
-ssh ceph002 'sudo rm -rf /etc/ceph'
-ssh ceph002 'sudo rm -rf /var/log/ceph'
-ssh ceph003 'sudo rm -rf /var/lib/ceph'
-ssh ceph003 'sudo rm -rf /etc/ceph'
-ssh ceph003 'sudo rm -rf /var/log/ceph'
+sudo rm /home/ceph/ceph*
+sudo rm -rf /var/lib/ceph/*
+sudo rm -rf /etc/ceph/*
+sudo rm -rf /var/log/ceph/*
+sudo rm -rf /etc/apt/sources.list.d/ceph.list
+ssh ceph002 'sudo rm -rf /var/lib/ceph/*'
+ssh ceph002 'sudo rm -rf /etc/ceph/*'
+ssh ceph002 'sudo rm -rf /var/log/ceph/*'
+ssh ceph002 'sudo rm -rf /etc/apt/sources.list.d/ceph.list'
+ssh ceph003 'sudo rm -rf /var/lib/ceph/*'
+ssh ceph003 'sudo rm -rf /etc/ceph/*'
+ssh ceph003 'sudo rm -rf /var/log/ceph/*'
+ssh ceph003 'sudo rm -rf /etc/apt/sources.list.d/ceph.list'
 ssh ceph002 'sudo reboot'
 ssh ceph003 'sudo reboot'
 sudo reboot
