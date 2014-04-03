579082d01e2a2dc34c76ee9751789dff8a742458
Christian Berendt <berendt@b1-systems.de>
2014-04-03 22:21:38 +0200

    removed :

--- openstack/ns_dhcp.sh
+++ openstack/ns_dhcp.sh
@@ -3,6 +3,6 @@
 # author: Christian Berendt <berendt@b1-systems.de>
 
 for ns in $(ip netns show | grep qdhcp); do
-  echo -n "$ns : "
+  echo -n "$ns "
   ip netns exec $ns ip a s | grep ns- | grep inet | grep -v 169 | awk '{ print $2 }'
 done
