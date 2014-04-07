85e34561c0628ebb17507100142dc8378d555d0c
Christian Berendt <berendt@b1-systems.de>
2014-04-07 19:26:29 +0200

    changed debugging level to INFO

--- cex.io/balances.py
+++ cex.io/balances.py
@@ -10,7 +10,7 @@ import time
 import yaml
 import sys
 
-logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
 from bigfloat import *
 precision(8)
